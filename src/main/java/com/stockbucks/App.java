a222756b37def13ba14748f98698411f5963b782
Ryder1207 <141506194+Ryder1207@users.noreply.github.com>
2026-05-25 17:46:34 +0800

    Update App.java
    將出發點改成WelcomeUI.java

--- src/main/java/com/stockbucks/App.java
+++ src/main/java/com/stockbucks/App.java
@@ -1,10 +1,20 @@
 package com.stockbucks;
 
-import com.stockbucks.gui.MainApp;
+import com.stockbucks.gui.WelcomeUI;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("正在啟動 StockBucks 系統...");
+        // 實例化歡迎介面並呈現
+        WelcomeUI welcomeUI = new WelcomeUI(primaryStage);
+        welcomeUI.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("正在啟動GUI");
-        MainApp.startApp(args);
+        launch(args);
     }
 }
